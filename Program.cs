09ef55a17a76297f65cf6a1bb767caef027722fc
Gusbod <skrapiskrap@gmail.com>
2021-09-16 13:17:42 +0200

    Clear test

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ int numberOfPeopleInList = 0;
 while (true)
 {
     Console.Clear();
+    Console.Clear();
     Console.ForegroundColor = ConsoleColor.Yellow;
     Console.WriteLine($"~: ADRESSBOKEN ({numberOfPeopleInList} personer, {contacts.Length} tecken) :~\n");
     Console.ResetColor();
