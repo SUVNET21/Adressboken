acbffcbf60b29d4020b24fd24fd66af174e81ecc
Daniel T <90195897+DTSD21@users.noreply.github.com>
2021-09-22 00:07:45 +0200

    put functions into methods, added skeleton for new functions

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace AdressbokEtt
         static List<string> nameList = new();
         static List<string> numberList = new();
         static char choice;
+        static bool extraChoice;
         //string contacts = "";
         //string numbers = "";
 
@@ -22,6 +23,69 @@ namespace AdressbokEtt
 
                 if (choice == 'l')
                 {
+                    AddName();
+                }
+                else if (choice == 'v')
+                {
+                    ShowNames();
+                }
+                else if (choice == 'r')
+                {
+                    ClearNames();
+                }
+                else if (choice == 'a')
+                {
+                    Console.Clear();
+                    Console.WriteLine("Ha en fin dag! Hejdå!");
+                    Environment.Exit(0);
+                }
+
+                else if (choice == 'n')
+                {
+                    AddNumber();
+                }
+
+                else if (choice == 's')
+                {
+                    ShowNumbers();
+                }
+                else if (choice == 't')
+                {
+                    ClearNumbers();
+                }
+                else if (choice == '1')
+                {
+                    SearchName();
+                }
+                else if (choice == '2')
+                {
+                    SearchNumber();
+                }
+            }
+        }
+
+        static void ShowMenu()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"~: Välkommen till din adressbok :~\n");
+            Console.WriteLine("Denna av version av adressboken kan spara både namn och telefonnummer.");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("[L]ägg till namn.\n[V]isa sparade namn.\n[R]ensa adressboken på namn.");
+            Console.WriteLine("[N]ytt telefonnummer.\n[S]kriv ut alla telefonnummer.\n[T]a bort alla telefonnummer.");
+            Console.WriteLine("[A]vsluta adressboken\n");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Nyhet! Extrafunktioner: [1] Sök efter ett namn. [2] Sök efter ett nummer.");
+            Console.ResetColor();
+        }
+
+
+        static char GetChoice()
+        {
+            return choice = Char.ToLower(Console.ReadKey(true).KeyChar);
+        }
+        static void AddName()
+        {
                     Console.Clear();
                     Console.Write("\nNamn: ");
                     string newPerson = Console.ReadLine();
@@ -29,9 +93,11 @@ namespace AdressbokEtt
                     {
                         nameList.Add(newPerson);
                     }
-                }
-                else if (choice == 'v')
-                {
+                    
+        }
+        
+        static void ShowNames()
+        {
                     if (nameList.Count == 0)
                     {
                         Console.Clear();
@@ -46,15 +112,17 @@ namespace AdressbokEtt
                         }
                     }
                     Console.ReadKey();
-                }
-                else if (choice == 'r')
-                {
-                    if (nameList.Count == 0)
+        }
+
+        static void ClearNames()
+        {
+        if (nameList.Count == 0)
                     {
                         Console.Clear();
                         Console.WriteLine("Du har inte sparat några namn.");
                         Console.ReadKey();
                     }
+                
 
                     else
                     {
@@ -82,17 +150,11 @@ namespace AdressbokEtt
                             Console.ReadKey();
                         }
                     }
-                }
-                else if (choice == 'a')
-                {
-                    Console.Clear();
-                    Console.WriteLine("Ha en fin dag! Hejdå!");
-                    Environment.Exit(0);
-                }
+        }
 
-                else if (choice == 'n')
-                {
-                    Console.Clear();
+        static void AddNumber()
+        {
+        Console.Clear();
                     Console.Write("\nTelefonnummer: ");
                     string newNumber = Console.ReadLine();
                     if (!string.IsNullOrWhiteSpace(newNumber))
@@ -105,10 +167,11 @@ namespace AdressbokEtt
                         Console.WriteLine("Du har matat in ett felaktigt telefonnummer. Använd bara siffror!");
                         Console.ReadKey();
                     }
-                }
+        }
 
-                else if (choice == 's')
-                {
+        static void ShowNumbers()
+        {
+        
                     if (numberList.Count == 0)
                     {
                         Console.Clear();
@@ -123,9 +186,11 @@ namespace AdressbokEtt
                         }
                     }
                     Console.ReadKey();
-                }
-                else if (choice == 't')
-                {
+        }
+
+        static void ClearNumbers()
+        {
+        
 
                     if (numberList.Count == 0)
                     {
@@ -136,8 +201,12 @@ namespace AdressbokEtt
 
                     else
                     {
+                        extraChoice = true;
+
+                        while (extraChoice == true)
+                        {
                         Console.Clear();
-                        Console.WriteLine("\nÄr du säker på att du vill ta bort alla telefonnummer? Detta går inte att ångra! Y/N)");
+                        Console.WriteLine("Är du säker på att du vill ta bort alla telefonnummer? Detta går inte att ångra! Y/N)");
                         GetChoice();
 
 
@@ -147,40 +216,38 @@ namespace AdressbokEtt
                             Console.WriteLine("Då rensar vi alla nummer.");
                             numberList.Clear();
                             Console.ReadKey();
+                            extraChoice=false;
                         }
                         else if (choice == 'n')
                         {
                             Console.Clear();
                             Console.WriteLine("Okej, då rensar vi inte!");
                             Console.ReadKey();
+                            extraChoice=false;
                         }
                         else
                         {
                             Console.Clear();
                             Console.WriteLine("Hoppsan! Nu gick något snett. Använd knapparna Y eller N för att svara.");
                             Console.ReadKey();
+                            
+                        }
                         }
                     }
-                }
-            }
         }
-
-        static void ShowMenu()
+        static void SearchName()
         {
-            Console.Clear();
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine($"~: Välkommen till din adressbok :~\n");
-            Console.WriteLine("Denna av version av adressboken kan spara både namn och telefonnummer.");
-            Console.ResetColor();
-            Console.WriteLine("[L]ägg till namn.\n[V]isa sparade namn.\n[R]ensa adressboken på namn.");
-            Console.WriteLine("[N]ytt telefonnummer.\n[S]kriv ut alla telefonnummer.\n[T]a bort alla telefonnummer.");
-            Console.WriteLine("[A]vsluta adressboken");
+        Console.Clear();
+        System.Console.WriteLine("Test message");
+        Console.ReadKey();
         }
 
-
-        static char GetChoice()
+        static void SearchNumber()
         {
-            return choice = Char.ToLower(Console.ReadKey(true).KeyChar);
+        Console.Clear();
+        System.Console.WriteLine("Test message");
+        Console.ReadKey();
         }
+
     }
 }
